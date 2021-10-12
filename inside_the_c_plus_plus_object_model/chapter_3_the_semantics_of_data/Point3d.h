09a60da4227bfe61e8b40fcb7e3e542f35df1188
kushuaiming <xm2534911078@outlook.com>
2021-10-12 23:23:58 +0800

    point2d point3d

--- inside_the_c_plus_plus_object_model/chapter_3_the_semantics_of_data/Point3d.h
+++
@@ -1,9 +0,0 @@
-class Point3d {
-public:
-    Point3d(float, float, float);
-    float X() const { return x; }
-    void X(float new_x) const { x = new_x; }
-    //...
-private:
-    float x, y, z;
-};
---
+++ inside_the_c_plus_plus_object_model/chapter_3_the_semantics_of_data/adding_polymorphism.cc
@@ -0,0 +1,44 @@
+class Point2d {
+ public:
+  Point2d(float x = 0.0, float y = 0.0) : _x(x), _y(y){};
+
+  float x() const { return _x; }
+  float y() const { return _y; }
+
+  void x(float newX) { _x = newX; }
+  void y(float newY) { _y = newY; }
+
+  virtual float z() const { return 0.0; }
+  virtual void z(float) {}
+
+  virtual void operator+=(const Point2d& rhs) {
+    _x += rhs.x();
+    _y += rhs.y();
+  }
+  // ... more members
+
+ protected:
+  float _x, _y;
+};
+
+class Point3d : public Point2d {
+ public:
+  Point3d(float x = 0.0, float y = 0.0, float z = 0.0) : Point2d(x, y), _z(z){};
+  float z() const { return _z; }
+  void z(float newZ) { _z = newZ; }
+
+  void operator+=(const Point2d& rhs) {
+    Point2d::operator+=(rhs);
+    _z += rhs.z();
+  }
+  // ... more members
+
+ protected:
+  float _z;
+};
+
+void foo(Point2d &p1, Point2d &p2) {
+  // ...
+  p1 += p2;
+  // ...
+}
